72e98bb9431db46029bbb96a56a928f16a35991f
Kevin Kusy <kkusy1971@gmail.com>
2026-04-04 12:21:04 -0500

    Added test

--- src/test/java/io/github/kkusylabs/hellospring/controller/HelloControllerIntegrationTest.java
+++ src/test/java/io/github/kkusylabs/hellospring/controller/HelloControllerIntegrationTest.java
@@ -1,14 +1,15 @@
 package io.github.kkusylabs.hellospring.controller;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Integration test for {@link HelloController}.
